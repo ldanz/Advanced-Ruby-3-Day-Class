ee82efadf6c0c96269e502d135a4531ea29f674e
Lisa Danz <lisa.danz@opower.com>
2014-10-01 10:49:19 -0700

    add path from room to room

--- DSL/game.rb
+++ DSL/game.rb
@@ -1,7 +1,14 @@
 i = 1
+
 COMMANDS =[]
 Rooms = []
 Paths = []
+
+Room = Struct.new(:name, :north, :east, :south, :west) do
+  def to_s
+    name
+  end
+end
 
 loop do
   print "#{i}> "
@@ -34,14 +41,40 @@ loop do
     when /\Acreate\z/i
       if params[0] == "room"
         print "Enter Room > "
-        room = gets
+        room = Room.new(gets.chomp)
         puts "You created #{room}"
         Rooms << room
       elsif params[0] == "path"
-        print "Enter Path > "
-        path = gets
-        puts "You created #{path}"
-        Paths << path
+        print "Enter starting room > "
+        room_name = gets.chomp
+        starting_room = Rooms.find { |room| room.name == room_name }
+        if starting_room.nil?
+          puts "No room named #{room_name} exists"
+          next
+        end
+
+        print "Enter destination room > "
+        room_name = gets.chomp
+        destination_room = Rooms.find { |room| room.name == room_name }
+        if destination_room.nil?
+          puts "No room named #{room_name} exists"
+          next
+        end
+
+        print "Enter direction > "
+        direction = gets.chomp.downcase
+        unless %w{north south east west}.include? direction
+          puts 'You must enter north, south, east, or west'
+          next
+        end
+
+        if starting_room.send(direction)
+          puts "#{starting_room} already has a neighbor to the #{direction}"
+          next
+        end
+
+        starting_room.send("#{direction}=", destination_room)
+        puts "You've added a path from #{starting_room}, heading #{direction}, to #{destination_room}."
       else
         puts "Create expects Room or Path"
       end
