0065cf859ef757edc737defe150ca379eaa85aec
Lisa Danz <lisa.danz@opower.com>
2014-10-01 09:56:06 -0700

    repl modifications

---
+++ DSL/game.rb
@@ -0,0 +1,34 @@
+i = 1
+COMMANDS =[]
+loop do
+  print "#{i}> "
+  i+=1
+  raw_input = gets
+  if raw_input.nil?
+    puts
+    break
+  else
+    input = raw_input.chomp
+  end
+  COMMANDS << input
+  command, *params = input.split /\s/
+  case command
+    when /\Ahelp\z/i
+      puts "+++"
+      puts "open #loads a file"
+      puts "exit #will exit"
+      puts "do <ruby code> #evals <ruby code>"
+      puts "anything else doesn't work"
+      puts "+++"
+    when /\Aopen\z/i
+      puts "should open #{params.first}"
+    when /\Ado\z/i
+      puts(eval(*params.join))
+    when /\Aexit\z/i
+      break
+    when /\Alast\z/i
+      puts COMMANDS[-2]
+    else
+      puts 'Invalid command'
+  end
+end
